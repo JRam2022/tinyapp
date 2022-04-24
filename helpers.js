22e7dd66a5d457cbed980ace4d2ce34043c77666
JRam2022 <jasonram778@gmail.com>
2022-04-23 19:03:00 -0700

    Remove test code

--- helpers.js
+++ helpers.js
@@ -1,7 +1,6 @@
 const generateRandomString = function() {
   // array of 52 letters for random generation
   const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  //console.log(alphabet.length)
   let randomStr = '';
   //generates 6 random indexs of alphabet between 0-62
   for (let i = 0; i < 6; i++) {
@@ -15,36 +14,25 @@ const generateRandomString = function() {
 const urlsForUser = function(id, database) {
   let matchingUrlID = {};
   for (url in database) {
-    //console.log('URL IN DATABASE', url)
-    //console.log('URL USER ID', urlDatabase[url]['userID'])
     if (database[url]['userID'] === id) {
-      //console.log('matched')
-      
       matchingUrlID[url] = {
-      longURL: database[url]['longURL'],
-      userID: database[url]['userID']
-      }
+        longURL: database[url]['longURL'],
+        userID: database[url]['userID']
+      };
     }
   }
   return matchingUrlID;
-}
+};
 
-//const database = urlDatabase
-//const email = '1@1.com'
+
 const getUserByEmail = function(email, database) {
-  // lookup magic...
-  const lookupObject = Object.values(database)
-  //console.log('OBJECT', lookupObject)
-  
+  const lookupObject = Object.values(database);
   for (const item of lookupObject) {
-    //console.log('ITEM', item)
-    //console.log('EMAIL', email)
     if (item['email'] === email) {
       return true;
-    } 
+    }
   }
   return false;
 };
 
-module.exports = { urlsForUser, getUserByEmail, generateRandomString }
-//getUserByEmail(email, database)
+module.exports = { urlsForUser, getUserByEmail, generateRandomString };
